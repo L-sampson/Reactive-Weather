2f8543ee1936f010991b72295a75f8aa1151878b
L-sampson <lavonsampson@google.com>
2023-10-23 17:53:24 -0400

    Created component for Location and weather icons

--- App.js
+++ App.js
@@ -2,14 +2,19 @@ import React from 'react';
 // Import data and WeatherCard here
 import cities from './data.js'
 import WeatherCard from './components/WeatherCard.js'
+import  {useState} from 'react';
+import Location from './components/Location.js'
 
 function App() {
+    const[location, setLocation] = useState("New York City")
     return (
         <>
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {cities.map((citiesInfo)=><WeatherCard data ={citiesInfo} />)}
+                <h3>Your Location's Weather</h3>
+                <Location data ={cities} location={location} setLocation ={setLocation} />
+                {/*cities.map((citiesInfo)=><WeatherCard data ={citiesInfo} />)*/}
             </div>
             
         </>
--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,12 +1,27 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg"
+import cloudy from "../assets/Cloudy.svg"
+import rainy from "../assets/Rainy.svg"
+import partlyCloudy from "../assets/PartlyCloudy.svg"
 
 
 function WeatherCard(props) {
+  const icon = ()=>{
+    switch(props.data.forecast){
+      case "Rainy":
+        return <img className="card-img-top" src = {rainy} alt="Card image cap" id = "icon"/>;
+      case "PartlyCloudy":
+        return <img className="card-img-top" src = {partlyCloudy} alt="Card image cap" id = "icon"/>;
+      case "Cloudy":
+        return <img className="card-img-top" src = {cloudy} alt="Card image cap" id = "icon"/>;
+      default:
+        return <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>;
+    }
+  };
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+          {icon()}      
         </div>
         <div class="card-body">
             <h3 className="card-title">{props.data.city}</h3>
