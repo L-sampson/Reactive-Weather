f0ff23ba07b4156d61583106e763bf4e343fed88
L-sampson <lavonsampson@google.com>
2023-10-25 17:40:33 -0400

    update data seed to have key. Prepping for APis.

--- data.js
+++ data.js
@@ -2,102 +2,122 @@ const cities = [
     {
       city: 'New York City',
       temperature: 14,
-      forecast: 'Sunny'
+      forecast: 'Sunny',
+      key: "14-349727_1_AL"
     },
     {
       city: 'London',
       temperature: 9,
-      forecast: 'Rainy'
+      forecast: 'Rainy',
+      key: "328328"
     },
     {
       city: 'Paris',
       temperature: 13,
-      forecast: 'Partly cloudy'
+      forecast: 'Partly cloudy',
+      key: "623"
     },
     {
       city: 'Tokyo',
       temperature: 18,
-      forecast: 'Cloudy'
+      forecast: 'Cloudy',
+      key: "226396"
     },
     {
       city: 'Sydney',
       temperature: 25,
-      forecast: 'Sunny'
+      forecast: 'Sunny',
+      key: "22889"
     },
     {
       city: 'Los Angeles',
       temperature: 22,
-      forecast: 'Partly cloudy'
+      forecast: 'Partly cloudy',
+      key: "347625"
     },
     {
       city: 'Berlin',
       temperature: 11,
-      forecast: 'Rainy'
+      forecast: 'Rainy',
+      key: "178087"
     },
     {
       city: 'Mumbai',
       temperature: 30,
-      forecast: 'Sunny'
+      forecast: 'Sunny',
+      key: "204842"
     },
     {
       city: 'Toronto',
       temperature: 8,
-      forecast: 'Snowy'
+      forecast: 'Snowy',
+      key: "55488"
     },
     {
       city: 'Dubai',
       temperature: 35,
-      forecast: 'Sunny'
+      forecast: 'Sunny',
+      key: "323091"
     },
     {
       city: 'Amsterdam',
       temperature: 12,
-      forecast: 'Rainy'
+      forecast: 'Rainy',
+      key: "249758"
     },
     {
       city: 'Shanghai',
       temperature: 19,
-      forecast: 'Cloudy'
+      forecast: 'Cloudy',
+      key: "106577"
     },
     {
       city: 'Mexico City',
       temperature: 20,
-      forecast: 'Partly cloudy'
+      forecast: 'Partly cloudy',
+      key: "242560"
     },
     {
       city: 'Rio de Janeiro',
       temperature: 27,
-      forecast: 'Sunny'
+      forecast: 'Sunny',
+      key: "45449"
     },
     {
       city: 'Moscow',
       temperature: 2,
-      forecast: 'Snowy'
+      forecast: 'Snowy',
+      key: "294021"
     },
     {
       city: 'Seoul',
       temperature: 16,
-      forecast: 'Cloudy'
+      forecast: 'Cloudy',
+      key: "226081"
     },
     {
       city: 'Cape Town',
       temperature: 18,
-      forecast: 'Sunny'
+      forecast: 'Sunny',
+      key: "306633"
     },
     {
       city: 'Vancouver',
       temperature: 6,
-      forecast: 'Rainy'
+      forecast: 'Rainy',
+      key: "53286"
     },
     {
       city: 'Rome',
       temperature: 14,
-      forecast: 'Partly cloudy'
+      forecast: 'Partly cloudy',
+      key: "213490" 
     },
     {
       city: 'Madrid',
       temperature: 15,
-      forecast: 'Sunny'
+      forecast: 'Sunny',
+      key: "308526"
     }
 ];
 
